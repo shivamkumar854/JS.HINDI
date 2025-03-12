1fd336ea80528334d136960cdb7b4da740773c3f
shivamkumar854 <shivamkumar3210hjp@gamil.com>
2025-03-12 22:27:43 +0530

    const and variable

---
+++ const,var.js
@@ -0,0 +1,18 @@
+const accountId ="12345"
+let accountEmail="shivam@gmail.com"
+let accountPassword="123456"
+accountCity="Delhi"
+ 
+ 
+accountEmail="sk@gmail.com"
+accountPassword="26534"
+accountCity="Mumbai"
+
+
+/*
+Prefer not to used var for variables
+becauseofissue in bock and fuctional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity])
+
+
---
+++ datatype.js
@@ -0,0 +1,28 @@
+"use strict"; //treat all JS code as strict mode code as newer versions of JS are strict by default
+
+// alert(3+3) // we are using node.js, not browser
+
+console.log(3
+    +3) //code readability should be high
+
+    console.log("shivam")
+
+    let name="shivam"
+    let age=23
+    let isLogged=false
+
+    // DATA TYPES
+
+    // number =>2 to power 53
+    // bigint=>used in big number like trading ,stock market
+    // string =>""
+    // boolean=>true/false
+    // null=>standlone value
+    // undefined=>
+    // symbol=>used in unique thing
+
+    // object=>key value pair
+
+    console.log(typeof "shivam")
+    console.log(typeof null) //object
+    console.log(typeof undefined) //undefined
