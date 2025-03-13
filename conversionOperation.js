73c28ca66d10fc4814ced478ec85ff1b1375d3f3
shivamkumar854 <shivamkumar3210hjp@gamil.com>
2025-03-14 01:03:25 +0530

    datatype summary

---
+++ conversionOperation.js
@@ -0,0 +1,24 @@
+ // *********** operation ***********
+
+
+ let value=10
+ let negValue=-value
+ //console.log(negValue);
+
+//console.log(10+10);
+//console.log(10-10);
+//console.log(10*10);
+//console.log(10/10);
+//console.log(10%10);
+//console.log(10**10);  
+
+let str1="shivam"
+let str2=" kumar"
+
+let str3=str1+str2
+console.log(str3);
+
+console.log("1" +2);
+console.log(1 + "2");
+console.log("1"+2+2);
+console.log(1+2+"2");
