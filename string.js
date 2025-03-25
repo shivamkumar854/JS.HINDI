24c0a99494af88916dd20db2e3892bb6387669fd
shivamkumar854 <shivamkumar3210hjp@gamil.com>
2025-03-25 22:55:54 +0530

    string

---
+++ string.js
@@ -0,0 +1,21 @@
+ const name = "shivam"
+ const repoCount = 50
+
+
+
+// console.log(name + repoCount + "value");
+// console.log(`Hello my name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('shivamsingh')
+// console.log(gameName[0]);
+// console.log(gameName._proto_);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+console.log(gameName.charAt(2));
+
+
+
+
+
+
